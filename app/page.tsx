f6965daf7f928b2e62749bbf49dbd38df3ea443c
juniorwebdev83 <juniorwebdev83@gmail.com>
2024-10-11 21:31:49 -0400

    10-11-24=-ai-added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main>
-      <p>Hello me again</p>
-    </main>
-  )
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
+
+const apiKey = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+});
+
+const generationConfig = {
+  temperature: 1,
+  topP: 0.95,
+  topK: 64,
+  maxOutputTokens: 8192,
+  responseMimeType: "text/plain",
+};
+
+export default function Page() {
+  async function run() {
+    const chatSession = model.startChat({
+      generationConfig,
+   // safetySettings: Adjust safety settings
+   // See https://ai.google.dev/gemini-api/docs/safety-settings
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage("Write a two lines zen story");
+
+
+    return result.response.text();
+  }
+
+  return <>{run()}</>;
+  
 }
+
