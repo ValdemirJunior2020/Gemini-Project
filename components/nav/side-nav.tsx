8b5e2f38133e4b3d5a6713a4755b37d1382d7a4c
ValdemirJunior2020 <your-infojr.83@gmail.com>
2024-10-16 13:33:21 -0400

    Side-Bar-statict

--- components/nav/side-nav.tsx
+++ components/nav/side-nav.tsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { LayoutDashboard, FileClock, WalletCards, Settings } from "lucide-react"
 
 export default function SideNav() {
+  const menu = [
+    {
+      name: 'Dashboard',
+      icon: LayoutDashboard,
+      path: '/dashboard'
+    },
+    {
+      name: 'History',
+      icon: FileClock,
+      path: '/dashboard/history'
+    },
+    {
+      name: 'Billing',
+      icon: WalletCards, // Fixed here
+      path: '/dashboard/billing'
+    },
+    {
+      name: 'Settings',
+      icon: Settings,
+      path: '/dashboard/settings'
+    }
+  ]
+
   return (
-    <div>
-      SideNav
+    <div className="h-screen p-5 shadow-sm border">
+      {menu.map((item, index) => (
+        <div key={index} className="flex m-2 mr-4 p-2 hover:bg-primary hover:text-white rounded-lg cursor-pointer">
+          <div className="flex">
+            <item.icon /> <span className="ml-2">{item.name}</span>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
+
+
+<LayoutDashboard />
